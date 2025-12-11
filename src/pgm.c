5787765380b6f3317423a7082b829b711b712f11
Camila Santiago <camila.santiago026@gmail.com>
2025-12-11 07:35:19 -0300

    docs: Remoção de comentários

--- src/pgm.c
+++ src/pgm.c
@@ -39,16 +39,13 @@ void writePGMImage(struct pgm *, char *);
 
 // int main(int argc, char *argv[]){
 // 	struct pgm img;
-
 // 	if (argc!=3){
 // 		printf("Formato: \n\t %s <imagemEntrada.pgm> <imagemSaida.pgm>\n",argv[0]);
 // 		exit(1);
 // 	}
-
 // 	readPGMImage(&img,argv[1]);
 // 	writePGMImage(&img, argv[2]);
 // 	viewPGMImage(&img);
-
 // 	return 0;
 // }
 
